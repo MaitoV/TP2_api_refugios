42556e01f6e1f50abcc0f01e697b95fb6b49864c
emimoretta <emimoretta@gmail.com>
2024-06-23 11:47:06 -0300

    agregado index y login

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import indexRouter from './router/index.js';
 import conexionMongoDB from './model/dbMongo.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 class Server {
     constructor(port, persistencia) {
@@ -15,6 +17,28 @@ class Server {
         //this.app.use(express.urlencoded({extended:true}));
         //this.app.use(express.static('public'));
         this.app.use(express.json());
+        this.app.use(express.static('/public'))
+        this.app.use(express.urlencoded({extended: true}))
+
+
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+
+        // Middleware para servir archivos estáticos
+        this.app.use(express.static(path.join(__dirname, 'public')));
+
+        // Configurar el motor de plantillas
+        this.app.set('views', path.join(__dirname, 'views'));
+        this.app.set('view engine', 'html');
+
+        // Ruta para renderizar la vista
+        this.app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'index.html'));
+        });
+
+        this.app.get('/login', (req, res) => {
+            res.sendFile(path.join(__dirname, 'views', 'login.html'));
+        });
 
         this.app.use('/api', new indexRouter().start());
         this.app.use(errorHandler);
